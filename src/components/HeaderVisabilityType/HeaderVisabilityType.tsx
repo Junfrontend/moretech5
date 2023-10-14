e4822987679a0e41e880dfb6487c53efc15bbc92
Asya Nikolskaya <nickolskaya@gmail.com>
2023-10-14 23:10:50 +0300

    Fix header nav buttons

--- src/components/HeaderVisabilityType/HeaderVisabilityType.tsx
+++ src/components/HeaderVisabilityType/HeaderVisabilityType.tsx
@@ -1,69 +1,81 @@
-import Box from "@mui/material/Box";
-import ToggleButton from "@mui/material/ToggleButton";
-import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { getDataDisplayType } from "../../redux/UserLocationSlice/selectors";
-import { setDataDisplayType } from "../../redux/UserLocationSlice/UserLocationSlice";
-import { DATA_DISPLAY_TYPE } from "../../types";
+import Box from '@mui/material/Box';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { getDataDisplayType } from '../../redux/UserLocationSlice/selectors';
+import { setDataDisplayType } from '../../redux/UserLocationSlice/UserLocationSlice';
+import { DATA_DISPLAY_TYPE } from '../../types';
 
 const HeaderVisabilityType = () => {
   const displayType = useAppSelector(getDataDisplayType);
   const dispatch = useAppDispatch();
 
   const buttonStyle = {
-    textTransform: "capitalize",
-    width: "100%",
+    textTransform: 'capitalize',
+    width: '100%',
     border: '2px solid var(--color-background) !important',
-    fontSize: "16px",
-    borderRadius: "14px !important",
-    fontWeight: "500",
+    fontSize: '16px',
+    borderRadius: '14px !important',
+    fontWeight: '500',
   };
 
   return (
     <Box
       sx={{
-        position: "absolute",
+        position: 'absolute',
         top: 0,
         right: 0,
         left: 0,
-        backgroundColor: "var(--color-background)",
-        p: "15px 10px",
-        zIndex: 10
+        backgroundColor: 'var(--color-background)',
+        p: '15px 10px',
+        zIndex: 10,
       }}
     >
       <ToggleButtonGroup
         value={displayType}
         exclusive
         onChange={(_, newValue) => dispatch(setDataDisplayType(newValue))}
-        aria-label="text alignment"
+        aria-label='text alignment'
         sx={{
-          width: "99%",
-          borderRadius: "16px",
-          border: "2px solid #1350AE",
-          outline: "4px solid var(--color-background)",
+          width: '99%',
+          borderRadius: '16px',
+          border: '2px solid #1350AE',
+          outline: '4px solid var(--color-background)',
         }}
       >
         <ToggleButton
           sx={{
             ...buttonStyle,
             backgroundColor:
-              displayType === DATA_DISPLAY_TYPE.LIST ? "#1350AE !important" : "var(--color-background)",
-            color: displayType === DATA_DISPLAY_TYPE.LIST ? "#FFF !important" :"#1350AE",
+              displayType === DATA_DISPLAY_TYPE.LIST
+                ? '#1350AE !important'
+                : 'var(--color-background)',
+            color:
+              displayType === DATA_DISPLAY_TYPE.LIST
+                ? '#FFF !important'
+                : '#1350AE',
           }}
+          disabled={displayType === DATA_DISPLAY_TYPE.LIST}
           value={DATA_DISPLAY_TYPE.LIST}
-          aria-label="left aligned"
+          aria-label='left aligned'
         >
           <span>Список</span>
         </ToggleButton>
         <ToggleButton
+          disabled={displayType === DATA_DISPLAY_TYPE.MAP}
           sx={{
             ...buttonStyle,
             backgroundColor:
-              displayType === DATA_DISPLAY_TYPE.MAP ? "#1350AE !important" : "var(--color-background)",
-            color: displayType === DATA_DISPLAY_TYPE.MAP ? "#FFF !important" :"#1350AE",
+              displayType === DATA_DISPLAY_TYPE.MAP
+                ? '#1350AE !important'
+                : 'var(--color-background)',
+            color:
+              displayType === DATA_DISPLAY_TYPE.MAP
+                ? '#FFF !important'
+                : '#1350AE',
           }}
           value={DATA_DISPLAY_TYPE.MAP}
-          aria-label="centered"
+          aria-label='centered'
         >
           <span>Карта</span>
         </ToggleButton>
